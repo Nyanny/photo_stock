538b2225720fe03955de12951efc104a8c8e5644
Nyanny <evgenynedosekin@gmail.com>
2022-11-09 11:47:14 +0300

    feat(SI-2071): added necessary constants

---
+++ lib/assets/app_styles.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// class [AppTextStyles] defines styles
+class AppTextStyles {
+  /// [manrope32h44w700Black] defines manrope font with color black, font size 32, line height 44, font weight 700
+  static final manrope32h44w700Black = GoogleFonts.manrope(
+    color: Colors.black,
+    fontSize: 32.0,
+    height: 44 / 32,
+    fontWeight: FontWeight.w700,
+  );
+
+  /// [manrope16h22Black] defines manrope font with color black, font size 16, line height 22, font weight 700
+  static final manrope16h22Black = GoogleFonts.manrope(
+    color: Colors.black,
+    fontSize: 16.0,
+    height: 22 / 16,
+    fontWeight: FontWeight.w700,
+  );
+
+  /// [manrope16h22w500White] defines manrope font with color white, font size 16, line height 22, font weight 500
+  static final manrope16h22w500White = GoogleFonts.manrope(
+    color: Colors.white,
+    fontSize: 16.0,
+    height: 22 / 12,
+    fontWeight: FontWeight.w500,
+  );
+
+  /// [manrope12h16w700White] defines manrope font with color white, font size 12, line height 16, font weight 700
+  static final manrope12h16w700White = GoogleFonts.manrope(
+    color: Colors.white,
+    fontSize: 12.0,
+    height: 16 / 12,
+    fontWeight: FontWeight.w700,
+  );
+
+  /// [manrope10h14w400White] defines manrope font with color white, font size 10, line height 14, font weight 400
+  static final manrope10h14w400White = GoogleFonts.manrope(
+    color: Colors.white,
+    fontSize: 10.0,
+    height: 14 / 10,
+    fontWeight: FontWeight.w400,
+  );
+}
---
+++ lib/assets/res_consts.dart
@@ -0,0 +1,5 @@
+/// Class [AppStrings] defines string constans in the app
+class AppStrings {
+  /// [backButtonTitle] defines the text inside the [BackButtonWidget]
+  static const backButtonTitle = '<\u{00A0}Back';
+}
