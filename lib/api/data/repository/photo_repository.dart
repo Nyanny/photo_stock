a673b5b6e2f18515f3dbd2309778c2964b50ca86
Nyanny <evgenynedosekin@gmail.com>
2022-11-11 19:58:41 +0300

    fix(SI-2070): fixed minor oversights

--- lib/api/data/repository/photo_repository.dart
+++ lib/api/data/repository/photo_repository.dart
@@ -2,20 +2,20 @@ import 'package:dio/dio.dart';
 import 'package:photo_stock/api/data/dio/exeption_handling/dio_exception.dart';
 import 'package:photo_stock/api/data/models/photo.dart';
 import 'package:photo_stock/api/photos_api.dart';
+import 'package:photo_stock/feature/di/getit_di.dart';
 import 'package:photo_stock/feature/domain/mappers/photos_mapper.dart';
 import 'package:photo_stock/feature/domain/repository/iphoto_repository.dart';
-import 'package:photo_stock/feature/di/getit_di.dart';
 
 /// Implementation of [IPhotoRepository].
 /// The [getPhotos] method returns [List<Photo>].
 /// The [pageNumber] parameter specifies the requested page number.
 class PhotoRepository implements IPhotoRepository {
-  final photosApi = getIt.get<PhotosApi>();
+  final _photosApi = getIt.get<PhotosApi>();
 
   @override
   Future<List<Photo>> getPhotos({int pageNumber = 1}) async {
     try {
-      final response = await photosApi.getPhotosFromAPI(pageNumber);
+      final response = await _photosApi.getPhotosFromAPI(pageNumber);
       final photos = PhotosMapper.photosDTOMapper(response);
       return photos;
     } on DioError catch (e) {
--- lib/api/photos_api.dart
+++ lib/api/photos_api.dart
@@ -1,21 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:photo_stock/api/data/dio/dio_client.dart';
-import 'package:photo_stock/feature/domain/model/photo_dto.dart';
 import 'package:photo_stock/feature/di/getit_di.dart';
+import 'package:photo_stock/feature/domain/model/photo_dto.dart';
 
 import 'config/dio_config.dart';
 
-/// An API class that works with DioClient using a [dioClient] instance.
+/// An API class that works with DioClient using a [_dioClient] instance.
 /// The [getPhotosFromAPI] method requests data from API, maps and returns as [List<PhotoDTO>]. Also rethrows exceptions.
 class PhotosApi {
   /// [DioClient] instance.
-  final dioClient = getIt.get<DioClient>();
+  final _dioClient = getIt.get<DioClient>();
 
   /// Requests data from API, maps responded data to [List<PhotoDTO>].
   /// Rethrow exception.
   Future<List<PhotoDTO>> getPhotosFromAPI(int pageNumber) async {
     try {
-      final Response response = await dioClient.get(
+      final Response response = await _dioClient.get(
         NetworkConstants.photosListUrl,
         queryParameters: {'page': pageNumber},
       );
