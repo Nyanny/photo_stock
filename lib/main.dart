a36cf9783fc80ba430645e939fa3a35507c9568b
Nyanny <evgenynedosekin@gmail.com>
2022-11-14 12:09:48 +0300

    feat(SI-2073): main.dart now has BlocProvider above MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_stock/assets/themes.dart';
+import 'package:photo_stock/feature/bloc/photos_bloc.dart';
 import 'package:photo_stock/feature/screens/photos_list_screen.dart';
-
 import 'feature/di/getit_di.dart';
 
 /// App's entry point.
@@ -15,9 +16,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: AppTheme.appTheme,
-      home: const PhotosListScreen(),
+    return BlocProvider(
+      create: (context) => getIt.get<PhotosBloc>()..add(PhotosFetchEvent()),
+      child: MaterialApp(
+        theme: AppTheme.appTheme,
+        home: const PhotosListScreen(),
+      ),
     );
   }
 }
