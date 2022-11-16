0196edab0102f5ef3eed4a7aa8c94dbe489f7474
Nyanny <evgenynedosekin@gmail.com>
2022-11-16 19:14:59 +0300

    feat(SI-2296): changed SliverGrid to PagedSliverGrid from infinite_scroll_pagination package.

--- lib/widgets/photos_grid.dart
+++ lib/widgets/photos_grid.dart
@@ -1,65 +1,50 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:photo_stock/api/data/models/photo.dart';
 import 'package:photo_stock/assets/app_sizes.dart';
-import 'package:photo_stock/feature/bloc/photos_bloc.dart';
 import 'package:photo_stock/widgets/photo_grid_item.dart';
 
-/// Class [PhotosGrid] is a [SliverGrid] with [SliverPadding] build by [BlocBuilder]
-/// Items in the table are [PhotoGridItem]
+/// Class [PhotosGrid] is a with [SliverPadding], child: [PagedSliverGrid]  with builder [PagedChildBuilderDelegate]
+/// Has a [pagingController] that rules the grid
 class PhotosGrid extends StatelessWidget {
-  const PhotosGrid({Key? key}) : super(key: key);
+  final PagingController<int, Photo> pagingController;
 
-  /// Depends on a caught state builds SliverGrid with [photos[index]] value or a snackBar with an Error
+  const PhotosGrid({required this.pagingController, Key? key})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<PhotosBloc, PhotosState>(
-      builder: (context, state) {
-        List<Photo> photos = [];
-        if (state is PhotosLoadingState && !state.isFirstFetch) {
-          photos = state.oldPhotos;
-        } else if (state is PhotosLoadedState) {
-          photos = state.photos;
-        } else if (state is PhotosErrorState) {
-          final snackBar = SnackBar(
-            content: Text(state.message),
-          );
+    return SliverPadding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: AppSizes.paddingHorizontal),
+      sliver: PagedSliverGrid<int, Photo>(
+        pagingController: pagingController,
+        showNewPageProgressIndicatorAsGridChild: false,
+        showNewPageErrorIndicatorAsGridChild: false,
+        showNoMoreItemsIndicatorAsGridChild: false,
+        builderDelegate: PagedChildBuilderDelegate<Photo>(
+            newPageProgressIndicatorBuilder: (_) => const _ProgressIndicator(),
+            itemBuilder: (_, item, __) {
+              return PhotoGridItem(photo: item);
+            }),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: AppSizes.paddingGridItems,
+          crossAxisSpacing: AppSizes.paddingGridItems,
+        ),
+      ),
+    );
+  }
+}
 
-          /// Prevents grid items disappear
-          Future.delayed(const Duration(milliseconds: 2), () {
-            ScaffoldMessenger.of(context).showSnackBar(snackBar);
-          });
-          List<Photo>? tempPhotos = state.photosBeforeError;
-          if (tempPhotos != null) {
-            photos = tempPhotos;
-          }
-        } else {
-          return const SliverToBoxAdapter(
-            child: SizedBox.shrink(),
-          );
-        }
-        return SliverPadding(
-          padding: const EdgeInsets.only(
-              left: AppSizes.paddingHorizontal,
-              right: AppSizes.paddingHorizontal,
-              bottom: 0),
-          sliver: SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              childCount: photos.length,
-              (context, index) {
-                return PhotoGridItem(
-                  photo: photos[index],
-                );
-              },
-            ),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: AppSizes.paddingGridItems,
-              crossAxisSpacing: AppSizes.paddingGridItems,
-            ),
-          ),
-        );
-      },
+class _ProgressIndicator extends StatelessWidget {
+  const _ProgressIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.only(bottom: 25, top: 41),
+      child: CupertinoActivityIndicator(),
     );
   }
 }
