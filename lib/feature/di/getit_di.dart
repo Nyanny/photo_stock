aa3cb0a7c43e918768419378fb7754bb6fb56d48
Nyanny <evgenynedosekin@gmail.com>
2022-11-16 19:12:42 +0300

    refactor(SI-2296): refactored to delete unnecessary bloc code

--- lib/feature/di/getit_di.dart
+++ lib/feature/di/getit_di.dart
@@ -2,7 +2,6 @@ import 'package:get_it/get_it.dart';
 import 'package:photo_stock/api/data/dio/dio_client.dart';
 import 'package:photo_stock/api/data/repository/photo_repository.dart';
 import 'package:photo_stock/api/photos_api.dart';
-import 'package:photo_stock/feature/bloc/photos_bloc.dart';
 
 /// GetIt instance registration.
 final getIt = GetIt.instance;
@@ -13,8 +12,4 @@ Future<void> dioSetup() async {
   getIt.registerSingleton<DioClient>(DioClient());
   getIt.registerSingleton<PhotosApi>(PhotosApi());
   getIt.registerSingleton<PhotoRepository>(PhotoRepository());
-
-  /// Bloc
-  getIt.registerSingleton<PhotosBloc>(PhotosBloc());
-
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:photo_stock/assets/themes.dart';
-import 'package:photo_stock/feature/bloc/photos_bloc.dart';
-import 'package:photo_stock/feature/screens/photos_list_screen.dart';
 import 'feature/di/getit_di.dart';
+import 'feature/screens/photos_list_screen/photos_list_screen.dart';
 
 /// App's entry point.
 Future<void> main() async {
@@ -16,12 +14,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt.get<PhotosBloc>()..add(PhotosFetchEvent()),
-      child: MaterialApp(
-        theme: AppTheme.appTheme,
-        home: const PhotosListScreen(),
-      ),
+    return MaterialApp(
+      theme: AppTheme.appTheme,
+      home: const PhotosListScreen(),
     );
   }
 }
